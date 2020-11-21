f22520691c94c7aa4b66c865591c38e35e587dbf
Hugh <acrohacking@gmail.com>
2020-11-21 23:11:01 +1300

    update

--- main.py
+++ main.py
@@ -19,8 +19,6 @@ import cmd
 import os
 import difflib
 
-
-
 class Palindromo():
     def Menu(self, arg):
         if arg <= 10:
@@ -35,14 +33,22 @@ class Palindromo():
                 if verify == 'y':
                     string_reversed = str(''.join(reversed(string)))
                     if string == string_reversed:
-                        print("Es Palindroma")
+                        print("Es un palíndromo")
                     else:
-                        diff = difflib.Differ()
-                        diff_str = list(difflib.ndiff(string, string_reversed))
-                        result = int(len(diff_str)) - int(len(string))
-                        print(result)
-                        print("No es Palindroma")
-                return string
+                        string_lines = string.splitlines()
+                        string_reversed_lines = string_reversed.splitlines()
+                        #diff_str = list(difflib.ndiff(string, string_reversed))
+                        diff_str = list(difflib.ndiff(string_lines, string_reversed_lines))
+                        result = len(diff_str) - len(string)
+
+                        #print(''.join(difflib.restore(diff_str, which=1)))
+                        #diff = difflib.Differ()
+                        #diff_str = diff.compare(string_lines, string_reversed_lines)
+                        #print(''.join(diff_str))
+
+
+                        print("Para que sea un palíndromo se deben agregar al menos {} caracteres".format(result).replace('-', ''))
+
             elif arg == 2:
                 self.lista = []
                 f = open("PALIN2.IN", "r")
@@ -55,16 +61,14 @@ class Palindromo():
                     string_reversed = str(''.join(reversed(string)))
                     print(len(string_reversed))
                     if string == string_reversed:
-                        print("Es Palindroma")
+                        print("Es un palíndromo")
                     else:
-                        diff = difflib.Differ()
-                        diff_str = diff.compare(string, string_reversed)
-                        #diff_str = list(difflib.ndiff(string, string_reversed))
-                        print(''.join(diff_str))
-                        #result = int(len(diff_str)) - int(len(string))
-                        #print(result)
-                        print("No es Palindroma")
-                return string
+                        string_lines = string.splitlines()
+                        string_reversed_lines = string_reversed.splitlines()
+                        diff_str = list(difflib.ndiff(string_lines, string_reversed_lines))
+                        result = int(len(diff_str)) - int(self.lista[0])
+                        print("Para que sea un palíndromo se deben agregar {} caracteres".format(result).replace('-', ''))
+
             elif arg == 3:
                 self.lista = []
                 f = open("PALIN3.IN", "r")
@@ -72,72 +76,75 @@ class Palindromo():
                     self.lista.append(linea)
                 f.close()
                 string = str(self.lista[1].replace('\n', ''))
-                #print(int(len(string)))
                 verify = input('el archivo posee un string de longitud {} desea continuar y/n :'.format(self.lista[0]))
                 if verify == 'y':
                     string_reversed = str(''.join(reversed(string)))
                     if string == string_reversed:
-                        print("Es Palindroma")
+                        print("Es un palíndromo")
                     else:
-                        diff_str = list(difflib.ndiff(string, string_reversed))
-                        result = int(len(string)) - int(len(diff_str))
-                        print(result)
-                        print("No es Palindroma")
-                return string
+                        string_lines = string.splitlines()
+                        string_reversed_lines = string_reversed.splitlines()
+                        diff_str = list(difflib.ndiff(string_lines, string_reversed_lines))
+                        result = int(len(diff_str[1])) - int(self.lista[0])
+                        print("Para que sea un palíndromo se deben agregar {} caracteres".format(result).replace('-', ''))
+
             elif arg == 4:
                 self.lista = []
                 f = open("PALIN4.IN", "r")
                 for linea in f:
                     self.lista.append(linea)
                 f.close()
-                string = self.lista[1]
+                string = str(self.lista[1].replace('\n', ''))
                 verify = input('el archivo posee un string de longitud {} desea continuar y/n :'.format(self.lista[0]))
                 if verify == 'y':
-                    if string == ''.join(reversed(string)):
-                        print("Es Palindroma")
+                    string_reversed = str(''.join(reversed(string)))
+                    if string == string_reversed:
+                        print("Es un palíndromo")
                     else:
-                        test = ''.join(reversed(string))
-                        diff = difflib.Differ()
-                        diff_str = diff.compare(string, test)
-                        #print(''.join(diff_str))
-                        print("No es Palindroma")
-                return string
+                        string_lines = string.splitlines()
+                        string_reversed_lines = string_reversed.splitlines()
+                        diff_str = list(difflib.ndiff(string_lines, string_reversed_lines))
+                        result = int(len(diff_str[1].replace('+ ', ''))) - int(self.lista[0])
+                        print("Para que sea un palíndromo se deben agregar {} caracteres".format(result).replace('-', ''))
+
             elif arg == 5:
                 self.lista = []
                 f = open("PALIN5.IN", "r")
                 for linea in f:
                     self.lista.append(linea)
                 f.close()
-                string = self.lista[1]
+                string = str(self.lista[1].replace('\n', ''))
                 verify = input('el archivo posee un string de longitud {} desea continuar y/n :'.format(self.lista[0]))
                 if verify == 'y':
-                    if string == ''.join(reversed(string)):
-                        print("Es Palindroma")
+                    string_reversed = str(''.join(reversed(string)))
+                    if string == string_reversed:
+                        print("Es un palíndromo")
                     else:
-                        test = ''.join(reversed(string))
-                        diff = difflib.Differ()
-                        diff_str = diff.compare(string, test)
-                        #print(''.join(diff_str))
-                        print("No es Palindroma")
-                return string
+                        string_lines = string.splitlines()
+                        string_reversed_lines = string_reversed.splitlines()
+                        diff_str = list(difflib.ndiff(string_lines, string_reversed_lines))
+                        result = int(len(diff_str)) - int(self.lista[0])
+                        print("Para que sea un palíndromo se deben agregar {} caracteres".format(result).replace('-', ''))
+
             elif arg == 6:
                 self.lista = []
                 f = open("PALIN6.IN", "r")
                 for linea in f:
                     self.lista.append(linea)
                 f.close()
-                string = self.lista[1]
+                string = str(self.lista[1].replace('\n', ''))
                 verify = input('el archivo posee un string de longitud {} desea continuar y/n :'.format(self.lista[0]))
                 if verify == 'y':
-                    if string == ''.join(reversed(string)):
-                        print("Es Palindroma")
+                    string_reversed = str(''.join(reversed(string)))
+                    if string == string_reversed:
+                        print("Es un palíndromo")
                     else:
-                        test = ''.join(reversed(string))
-                        diff = difflib.Differ()
-                        diff_str = diff.compare(string, test)
-                        #print(''.join(diff_str))
-                        print("No es Palindroma")
-                return string
+                        string_lines = string.splitlines()
+                        string_reversed_lines = string_reversed.splitlines()
+                        diff_str = list(difflib.ndiff(string_lines, string_reversed_lines))
+                        result = int(len(diff_str)) - int(self.lista[0])
+                        print("Para que sea un palíndromo se deben agregar {} caracteres".format(result).replace('-', ''))
+
             elif arg == 7:
                 self.lista = []
                 f = open("PALIN7.IN", "r")
@@ -148,72 +155,81 @@ class Palindromo():
                 verify = input('el archivo posee un string de longitud {} desea continuar y/n :'.format(self.lista[0]))
                 if verify == 'y':
                     if str(string) == ''.join(reversed(string)):
-                        print("Es Palindroma")
+                        print("Es un palíndromo")
                     else:
-                        test = ''.join(reversed(string))
-                        print(test)
-                        diff = difflib.Differ()
+                        string_reversed = ''.join(reversed(string))
+
+                        #diff = difflib.Differ()
                         #diff_str = diff.compare(string, test)
                         #print(''.join(diff_str))
-                        diff_str = list(difflib.ndiff(string, test))
+
+                        #string_lines = string.splitlines()
+                        #string_reversed_lines = string_reversed.splitlines()
+                        #diff_str = list(difflib.ndiff(string_lines, string_reversed_lines))
+
+                        diff_str = list(difflib.ndiff(string, string_reversed))
+                        print(diff_str)
+
                         result = len(diff_str) - len(string)
-                        print(result)
-                        print("No es Palindroma")
-                return string
+                        print("Para que sea un palíndromo se deben agregar {} caracteres".format(result).replace('-', ''))
+
             elif arg == 8:
                 self.lista = []
                 f = open("PALIN8.IN", "r")
                 for linea in f:
                     self.lista.append(linea)
                 f.close()
-                string = self.lista[1]
+                string = str(self.lista[1].replace('\n', ''))
                 verify = input('el archivo posee un string de longitud {} desea continuar y/n :'.format(self.lista[0]))
                 if verify == 'y':
-                    if string == ''.join(reversed(string)):
-                        print("Es Palindroma")
+                    string_reversed = str(''.join(reversed(string)))
+                    if string == string_reversed:
+                        print("Es un palíndromo")
                     else:
-                        test = ''.join(reversed(string))
-                        diff = difflib.Differ()
-                        diff_str = diff.compare(string, test)
-                        #print(''.join(diff_str))
-                        print("No es Palindroma")
-                return string
+                        string_lines = string.splitlines()
+                        string_reversed_lines = string_reversed.splitlines()
+                        diff_str = list(difflib.ndiff(string_lines, string_reversed_lines))
+                        result = int(len(diff_str)) - int(self.lista[0])
+                        print("Para que sea un palíndromo se deben agregar {} caracteres".format(result).replace('-', ''))
+
             elif arg == 9:
                 self.lista = []
                 f = open("PALIN9.IN", "r")
                 for linea in f:
                     self.lista.append(linea)
                 f.close()
-                string = self.lista[1]
+                string = str(self.lista[1].replace('\n', ''))
                 verify = input('el archivo posee un string de longitud {} desea continuar y/n :'.format(self.lista[0]))
                 if verify == 'y':
-                    if string == ''.join(reversed(string)):
-                        print("Es Palindroma")
+                    string_reversed = str(''.join(reversed(string)))
+                    if string == string_reversed:
+                        print("Es un palíndromo")
                     else:
-                        test = ''.join(reversed(string))
-                        diff = difflib.Differ()
-                        diff_str = diff.compare(string, test)
-                        #print(''.join(diff_str))
-                        print("No es Palindroma")
-                return string
+                        string_lines = string.splitlines()
+                        string_reversed_lines = string_reversed.splitlines()
+                        diff_str = list(difflib.ndiff(string_lines, string_reversed_lines))
+                        result = int(len(diff_str)) - int(self.lista[0])
+                        print("Para que sea un palíndromo se deben agregar {} caracteres".format(result).replace('-', ''))
+
             elif arg == 10:
                 self.lista = []
                 f = open("PALIN10.IN", "r")
                 for linea in f:
                     self.lista.append(linea)
                 f.close()
-                string = self.lista[1]
+                string = str(self.lista[1].replace('\n', ''))
                 verify = input('el archivo posee un string de longitud {} desea continuar y/n :'.format(self.lista[0]))
                 if verify == 'y':
-                    if string == ''.join(reversed(string)):
-                        print("Es Palindroma")
+                    string_reversed = str(''.join(reversed(string)))
+                    if string == string_reversed:
+                        print("Es un palíndromo")
                     else:
-                        test = ''.join(reversed(string))
-                        diff = difflib.Differ()
-                        diff_str = diff.compare(string, test)
-                        #print(''.join(diff_str))
-                        print("No es Palindroma")
-                return string
+                        string_lines = string.splitlines()
+                        string_reversed_lines = string_reversed.splitlines()
+                        diff_str = list(difflib.ndiff(string_lines, string_reversed_lines))
+                        result = int(len(diff_str)) - int(self.lista[0])
+                        print("Para que sea un palíndromo se deben agregar {} caracteres".format(result).replace('-', ''))
+
         else:
             msg = 'choice invalid'
             return msg
@@ -226,3 +242,4 @@ print('Seleccione la opcion de archivo en el que desea trabajar:')
 print('\n 1) - PALIN1.IN: \n 2) - PALIN2.IN: \n 3) - PALIN3.IN: \n 4) - PALIN4.IN: \n 5) - PALIN5.IN: \n 6) - PALIN6.IN: \n 7) - PALIN7.IN: \n 8) - PALIN8.IN: \n 9) - PALIN9.IN: \n 10) - PALIN10.IN: ')
 choice = input('choice: ')
 result = pl.Menu(int(choice))
+print(result)
